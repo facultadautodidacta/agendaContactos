4f8171eb44134b8960fac71ebc3d076de0266056
facultad <roldan9091@gmail.com>
2021-01-12 00:49:19 -0600

    agregado de dependencias

---
+++ dependencias.php
@@ -0,0 +1,13 @@
+
+
+	<link rel="stylesheet" type="text/css" href="public/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="public/fontawesome/css/all.css">
+	<link rel="stylesheet" type="text/css" href="public/datatable/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="public/datatable/dataTables.bootstrap4.min.css">
+
+	<script src="public/jquery-3.5.1.min.js"></script>
+	<script src="public/bootstrap/popper.min.js"></script>
+	<script src="public/bootstrap/bootstrap.min.js"></script>
+	<script src="public/datatable/jquery.dataTables.min.js"></script>
+	<script src="public/datatable/dataTables.bootstrap4.min.js"></script>
+	<script src="public/sweetalert.min.js"></script>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Agenda de contactos web</title>
+	<link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php require_once "dependencias.php"; ?>
+</head>
+<body>
+
+</body>
+</html>
