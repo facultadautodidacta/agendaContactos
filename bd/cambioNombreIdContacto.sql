e6116c998482c01b50b4af1194542e922dee521c
facultad <roldan9091@gmail.com>
2021-02-16 15:44:41 -0600

    mostrar datos de contactos

---
+++ bd/cambioNombreIdContacto.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `agendafa`.`t_contactos` 
+CHANGE COLUMN `id_agenda` `id_contacto` INT(11) NOT NULL ;
--- vistas/contactos/tablaContactos.php
+++ vistas/contactos/tablaContactos.php
@@ -1,3 +1,25 @@
+
+<?php 
+	require_once "../../clases/Conexion.php";
+	$con = new Conexion();
+	$conexion = $con->conectar();
+
+	$sql = "SELECT 
+					    contactos.paterno AS paterno,
+					    contactos.materno AS materno,
+					    contactos.nombre AS nombre,
+					    contactos.telefono AS telefono,
+					    contactos.email AS email,
+					    categorias.nombre AS categoria,
+					    contactos.id_contacto AS idContacto
+					FROM
+					    t_contactos AS contactos
+					        INNER JOIN
+					    t_categorias AS categorias 
+					ON contactos.id_categoria = categorias.id_categoria";
+	$result = mysqli_query($conexion, $sql);
+ ?>
+
 <div class="card">
 	<div class="card-body">
 		<div class="table-responsive">
@@ -15,24 +37,29 @@
 					</tr>
 				</thead>
 				<tbody>
+					<?php 
+						while($mostrar = mysqli_fetch_array($result)) {  
+							$idContacto = $mostrar['idContacto'];
+					?>
 					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
+						<td><?php echo $mostrar['paterno'] ?></td>
+						<td><?php echo $mostrar['materno'] ?></td>
+						<td><?php echo $mostrar['nombre'] ?></td>
+						<td><?php echo $mostrar['telefono'] ?></td>
+						<td><?php echo $mostrar['email'] ?></td>
+						<td><?php echo $mostrar['categoria'] ?></td>
 						<td>
 							<span class="btn btn-warning btn-sm" data-toggle="modal" data-target="#modalActualizarContacto">
 								<span class="fas fa-edit"></span>
 							</span>
 						</td>
 						<td>
-							<span class="btn btn-danger btn-sm" onclick="eliminarContacto()">
+							<span class="btn btn-danger btn-sm" onclick="eliminarContacto('<?php echo $idContacto ?>')">
 								<span class="far fa-trash-alt"></span>
 							</span>
 						</td>
 					</tr>
+					<?php } ?>
 				</tbody>
 			</table>
 		</div>
